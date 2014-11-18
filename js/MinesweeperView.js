578dcc097ab69ceab7d8f7449c96aafc71d9342f
Johnathan Weisner <joweisner@gmail.com>
2014-11-18 14:12:34 -0500

    Build board with render()

--- js/MinesweeperView.js
+++ js/MinesweeperView.js
@@ -2,3 +2,12 @@ var MinesweeperView = function (options) {
   var defaultOpts = { board: [[0,0,0],[0,0,0],[0,0,0]] };
   this.options = $.extend(defaultOpts, options);
 };
+
+MinesweeperView.prototype.render = function(){
+  for( var x = 0; x < this.options.board.length; x++ ) {
+    $('#minesweeper').append("<div class='row' data-row='" + x + "'></div>");
+    for( var y = 0; y < this.options.board[0].length; y++) {
+      $("#minesweeper .row[data-row='" + x + "']").append("<div class='cell' data-col='" + y + "'></div>")
+    }
+  }
+};
