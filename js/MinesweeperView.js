1d2f9b2423a3a0ebb8bbba8401c16844b4c23e4f
Johnathan Weisner <joweisner@gmail.com>
2014-11-18 15:36:20 -0500

    click board and it shows the cell value

--- js/MinesweeperView.js
+++ js/MinesweeperView.js
@@ -1,6 +1,7 @@
 var MinesweeperView = function (options) {
   var defaultOpts = { board: [[0,0,0],[0,0,0],[0,0,0]], cellSize: 50 };
   this.options = $.extend(defaultOpts, options);
+  this.initEventHandlers();
 };
 
 MinesweeperView.prototype.render = function(){
@@ -18,4 +19,19 @@ MinesweeperView.prototype.render = function(){
 
 MinesweeperView.prototype.updateCell = function(x, y, value) {
   $("#minesweeper .row[data-row='" + x + "'] .cell[data-col='" + y + "']").text(value);
-}
+};
+
+MinesweeperView.prototype.initEventHandlers = function() {
+  this.clickCell();
+};
+
+MinesweeperView.prototype.clickCell = function() {
+  var board = this.options.board;
+  $("#minesweeper").on("click", ".row .cell", function(e){
+    target = e.target;
+    e.preventDefault();
+    var row = parseInt($(target).parent().data("row"))
+    var col = parseInt($(target).data("col"))
+    this.updateCell(row, col, board[row][row]);
+  }.bind(this));
+};
