788c66a2afc330a5e4874dfad34b162d0da58337
Johnathan Weisner <joweisner@gmail.com>
2014-11-18 20:29:31 -0500

    Recursively opening up cells works

--- js/MinesweeperController.js
+++ js/MinesweeperController.js
@@ -10,44 +10,52 @@ MinesweeperController.prototype.run = function(){
 
 MinesweeperController.prototype.cellClicked = function() {
   $("#minesweeper").on("click", ".row .cell", function(e){
-    target = e.target;
-    console.log(target);
     e.preventDefault();
+    target = e.target;
     var row = parseInt($(target).parent().data("row"));
     var col = parseInt($(target).data("col"));
     var cell = this.opts.model.board[row][row];
-    console.log(cell);
-    if(cell.clicked === false) {
-      this.opts.view.updateCell(row, col, cell.value);
-      if(cell.value == 0){
-        this.openCellsAround(row,col)
-      }
+
+    if(cell.clicked === false && cell.value === 0) {
+      this.openCellsAround(row,col);
+    } else {
+      this.updateCell(row, col);
     }
+
   }.bind(this));
 };
 
 MinesweeperController.prototype.openCellsAround = function(x,y) {
   console.log("cells around " + x + ", " + y)
-  var width = this.opts.model.board.length
-  var height = this.opts.model.board[0].length
-  if(this.opts.model.board[x][y].clicked === true) return;
-  console.log(this.opts.model.board);
-  this.opts.model.board[x][y].clicked = true;
-  console.log(this.opts.model.board);
-  for( var x_i = -1; x_i < 2; x_i++) {
-    for( var y_i = -1; y_i < 2; y_i++) {
-      var new_x = x + x_i, new_y = y + y_i;
-      console.log("cells around " + new_x + ", " + new_y)
-      if (new_x < height && new_x > 0 && new_y < width && new_y > 0) {
-        var clicked = this.opts.model.board[new_x][new_y].clicked;
-        var value = this.opts.model.board[new_x][new_y].value;
-        this.opts.view.updateCell(new_x, new_y, 0);
-        console.log(clicked);
-        console.log(value);
-        if (value === 0) {
+  var startCell = this.opts.model.board[x][y];
+  if(startCell.clicked === true) return;
+
+  this.updateCell(x, y, startCell.value);
+  if(startCell.value != 9 && startCell.value != 0) return;
+  
+
+  for( var x_i = -1; x_i <= 1 ; x_i++) {
+    for( var y_i = -1; y_i <= 1; y_i++) {
+      var new_x = x + x_i;
+      var new_y = y + y_i;
+      if( this.inBounds(new_x, new_y) ) {
+        var cell = this.opts.model.board[new_x][new_y];
+        if ( this.inBounds(new_x, new_y) && cell.value === 0 && cell.clicked === false) {
+          this.opts.view.updateCell(new_x, new_y);
           this.openCellsAround(new_x, new_y);
         }
       }
     }
   }
+}
+
+MinesweeperController.prototype.updateCell = function(x, y) {
+  this.opts.model.board[x][y].clicked = true;
+  this.opts.view.updateCell(x, y, this.opts.model.board[x][y].value);
+}
+
+MinesweeperController.prototype.inBounds = function(x,y) {
+  var width = this.opts.model.board.length
+  var height = this.opts.model.board[0].length
+  return (x < height && x > -1 && y < width && y > -1)
 }
--- js/MinesweeperModel.js
+++ js/MinesweeperModel.js
@@ -1,15 +1,59 @@
-Array.prototype.repeat= function(default_value, length){
- while(length) this[--length]= default_value;
- return this;
+var MinesweeperModel = function (opts) {
+  var defaultOpts = { width: 9, height: 9 };
+  this.opts = $.extend(defaultOpts, opts);
+  this.opts.mines = (this.opts.width * this.opts.height) / 8
+  this.board = this.randomBoard();
+};
+
+MinesweeperModel.prototype.randomBoard = function() {
+  var board = [];
+  var coords = this.mineCoords();
+  for(var x = 0; x < this.opts.height; x++){
+    board.push([]);
+    for(var y = 0; y < this.opts.width; y++){
+      var value = 0;
+      var index = this.coordIn([x,y], coords)
+      if(index != -1){
+        value = 9;
+      };
+      board[x].push(new Cell(value));
+    } 
+  }
+  return board;
 }
 
-var MinesweeperModel = function (options) {
-  var defaultOpts = { width: 10, height: 10 };
-  this.options = $.extend(defaultOpts, options);
-  this.board = [].repeat([].repeat(new Cell(0),this.options.width), this.options.height);
-};
+MinesweeperModel.prototype.randomCoord = function() {
+  var x = Math.floor((Math.random() * this.opts.height));
+  var y = Math.floor((Math.random() * this.opts.width));
+  return [x, y];
+}
+
+MinesweeperModel.prototype.mineCoords = function() {
+  var coord = this.randomCoord();
+  var coords = [];
+  for(var i = 0; i < this.opts.mines; i++){
+    while(this.coordIn(coord, coords) != -1){
+      coord = this.randomCoord();
+    }
+    coords.push(coord);
+  }
+  return coords;
+}
+
+MinesweeperModel.prototype.compareCoords = function(a,b) {
+  return a[0] === b[0] && a[1] === b[1]
+}
+
+MinesweeperModel.prototype.coordIn = function(coord, array) {
+  for(var x = 0; x < array.length; x++) {
+    if(this.compareCoords(coord, array[x])) {
+      return x;
+    }
+  }
+  return -1;
+}
 
 var Cell = function(value) {
   this.clicked = false;
-  this.value = 0;
+  this.value = value;
 }
