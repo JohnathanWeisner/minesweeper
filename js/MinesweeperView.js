b0de81e2191d628976ae7e6f6626f3baf3a76086
Johnathan Weisner <joweisner@gmail.com>
2014-11-19 22:20:16 -0500

    Update styling

--- js/MinesweeperView.js
+++ js/MinesweeperView.js
@@ -13,11 +13,19 @@ MinesweeperView.prototype.render = function(){
   for( var x = 0; x < this.board.length ; x++ ) {
     $('#minesweeper').append("<ul class='row' data-row='" + x + "'></div>");
     for( var y = 0; y < this.board[0].length; y++) {
-      $("#minesweeper .row[data-row='" + x + "']").append("<li class='cell' data-col='" + y + "'></div>");
+      $("#minesweeper .row[data-row='" + x + "']").append("<li class='cell unclicked' data-col='" + y + "'><span></span></div>");
     }
   }
 };
 
 MinesweeperView.prototype.updateCell = function(x, y, value) {
-  $("#minesweeper .row[data-row='" + x + "'] .cell[data-col='" + y + "']").text(value);
+  $cellValue = $("#minesweeper .row[data-row='" + x + "'] .cell[data-col='" + y + "'] span")
+  $cell = $("#minesweeper .row[data-row='" + x + "'] .cell[data-col='" + y + "']")
+  if(value != 0 && value != 9) {
+    $cellValue.text(value);
+  } else if (value === 9) {
+    $cell.addClass("bomb");
+  }
+  $cell.removeClass("unclicked");
+  $cell.addClass("clicked");
 };
